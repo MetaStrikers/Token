5b9fe3dda361eed68dd87a45a0985a998a7695db
RenKoya1 <koya.ren.701@gmail.com>
2023-02-04 10:15:54 +0900

    fix format

--- contracts/Token/UToken.sol
+++ contracts/Token/UToken.sol
@@ -8,73 +8,73 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract UToken is ERC20, Ownable, AccessControl {
-    uint96 public transferFee;
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    mapping(address => bool) public taxExemptedSenders; // 　If from address is true, the tax is exempt.
-    mapping(address => bool) public taxExemptedReceivers; // If to address is true, the tax is exempt.
+	uint96 public transferFee;
+	bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+	mapping(address => bool) public taxExemptedSenders; // 　If from address is true, the tax is exempt.
+	mapping(address => bool) public taxExemptedReceivers; // If to address is true, the tax is exempt.
 
-    constructor(
-        uint256 initialSupplyToOwner,
-        uint96 _transferFee,
-        string memory name,
-        string memory symbol
-    ) ERC20(name, symbol) {
-        ERC20._mint(msg.sender, initialSupplyToOwner);
-        transferFee = _transferFee; // 100 = 1%
-        taxExemptedSenders[msg.sender] = true;
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(MINTER_ROLE, msg.sender);
-    }
+	constructor(
+		uint256 initialSupplyToOwner,
+		uint96 _transferFee,
+		string memory name,
+		string memory symbol
+	) ERC20(name, symbol) {
+		ERC20._mint(msg.sender, initialSupplyToOwner);
+		transferFee = _transferFee; // 100 = 1%
+		taxExemptedSenders[msg.sender] = true;
+		_setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+		_setupRole(MINTER_ROLE, msg.sender);
+	}
 
-    function setTransferFee(uint96 fee) public onlyOwner {
-        transferFee = fee;
-    }
+	function setTransferFee(uint96 fee) public onlyOwner {
+		transferFee = fee;
+	}
 
-    function setExemptedSenders(
-        address exemptedSenderAddress,
-        bool dutyFee
-    ) public onlyOwner {
-        taxExemptedSenders[exemptedSenderAddress] = dutyFee;
-    }
+	function setExemptedSenders(
+		address exemptedSenderAddress,
+		bool dutyFee
+	) public onlyOwner {
+		taxExemptedSenders[exemptedSenderAddress] = dutyFee;
+	}
 
-    function setExemptedReceivers(
-        address exemptedReceiversAddress,
-        bool dutyFee
-    ) public onlyOwner {
-        taxExemptedReceivers[exemptedReceiversAddress] = dutyFee;
-    }
+	function setExemptedReceivers(
+		address exemptedReceiversAddress,
+		bool dutyFee
+	) public onlyOwner {
+		taxExemptedReceivers[exemptedReceiversAddress] = dutyFee;
+	}
 
-    function _transfer(
-        address _sender,
-        address _recipient,
-        uint256 _amount
-    ) internal override {
-        if (taxExemptedSenders[_sender] || taxExemptedReceivers[_recipient]) {
-            super._transfer(_sender, _recipient, _amount);
-        } else {
-            uint256 amoutWithoutFee = _taxPayment(_sender, _amount);
-            super._transfer(_sender, _recipient, amoutWithoutFee);
-        }
-    }
+	function _transfer(
+		address _sender,
+		address _recipient,
+		uint256 _amount
+	) internal override {
+		if (taxExemptedSenders[_sender] || taxExemptedReceivers[_recipient]) {
+			super._transfer(_sender, _recipient, _amount);
+		} else {
+			uint256 amoutWithoutFee = _taxPayment(_sender, _amount);
+			super._transfer(_sender, _recipient, amoutWithoutFee);
+		}
+	}
 
-    function _taxPayment(
-        address _sender,
-        uint256 _amount
-    ) internal returns (uint256) {
-        uint256 feeAmount = (_amount * transferFee) / 10000;
-        super._transfer(_sender, owner(), feeAmount);
-        _amount -= feeAmount;
-        return _amount;
-    }
+	function _taxPayment(
+		address _sender,
+		uint256 _amount
+	) internal returns (uint256) {
+		uint256 feeAmount = (_amount * transferFee) / 10000;
+		super._transfer(_sender, owner(), feeAmount);
+		_amount -= feeAmount;
+		return _amount;
+	}
 
-    function mintTo(
-        address receiver,
-        uint256 supply
-    ) public onlyRole(MINTER_ROLE) {
-        _mint(receiver, supply);
-    }
+	function mintTo(
+		address receiver,
+		uint256 supply
+	) public onlyRole(MINTER_ROLE) {
+		_mint(receiver, supply);
+	}
 
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
-    }
+	function burn(uint256 amount) public {
+		_burn(msg.sender, amount);
+	}
 }
